63cf760825be16dae4e73f3d506cd01ed1dfe4c1
jhirvinmago <jhirvinsanandres@gmail.com>
2024-10-04 09:24:28 +0800

    Add files via upload

---
+++ .02BasicOutput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	cout << " Our Lady of Lourdes Collage foundation" << endl;
+	cout << " Collage of Computer Studies " << endl;
+	cout << " BSIT & 1st year " << endl;
+	cout << " Jhirvin S. Mago " << endl;
+	cout << " Purok-6 BRGY Guinacutan labo Camarines Norte " << endl;
+	return 0;
+}
+
+	
