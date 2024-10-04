32c914e737a628052e75fbe9b9d178db1ecb7465
jhirvinmago <jhirvinsanandres@gmail.com>
2024-10-04 09:25:09 +0800

    Add files via upload

---
+++ .04BasicInput.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	int course, school;
+	string name;
+	
+	cout << " Nickname: ";
+	cin >> name;
+	// only number not leter in course level
+	cout << " Course-Year Level:";
+	cin >> course;
+	cout << " School: ";
+	cin >> school ;
+	return 0;
+}
+
+
+
+	
